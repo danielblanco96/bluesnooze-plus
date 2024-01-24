3850c05f2be4790ba59b27fe7dd173fd3b47e13d
Daniel Blanco Calviño <danielblancocalvino@Daniels-MacBook-Pro.local>
2024-01-24 20:45:07 +0000

    Create xcode app project

---
+++ bluesnooze-plus/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  bluesnooze-plus
+//
+//  Created by Daniel Blanco Calviño on 24/1/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
